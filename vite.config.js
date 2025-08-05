6ff5aaa7dbc0b27eb20f7d6a307214d08dbd90b3
Ghug <ptp112004@gmail.com>
2025-08-05 16:15:26 +0700

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base:  "./",
-})
+export default {
+  base: process.env.VITE_BASE_PATH || "./",
+  // ...other config
+};
