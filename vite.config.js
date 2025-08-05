e021c207758c8e5e8ecb83d0cbcb1a920c575131
Ghug <ptp112004@gmail.com>
2025-08-05 16:59:15 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
-export default {
-  base: process.env.VITE_BASE_PATH || "./",
-  // ...other config
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
